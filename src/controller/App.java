536e453d154768fe890432691483e1a6dee9b92f
rudolphh <rudolpharthur@gmail.com>
2020-03-25 00:24:14 -0700

    Set up locale and resource bundle

--- src/controller/App.java
+++ src/controller/App.java
@@ -1,23 +1,70 @@
 package controller;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.util.Locale;
+import java.util.Optional;
+import java.util.ResourceBundle;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/login.fxml"));
-        primaryStage.setTitle("Customer Scheduling");
-        primaryStage.setScene(new Scene(root, 500, 300));
-        primaryStage.show();
-    }
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/login.fxml"));
+
+        ResourceBundle resourceBundle = getResourceBundle();
+        loader.setResources(resourceBundle);
+        Parent root = loader.load();
+
+        primaryStage.setTitle(resourceBundle.getString("title"));
+        primaryStage.setResizable(false);// for login screen
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
-}
+
+
+    // Helpers
+    static Optional<ButtonType> dialog(Alert.AlertType alertType, String title, String header, String content){
+        Alert alert = new Alert(alertType);
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        return alert.showAndWait();
+    }
+
+    static void closeThisWindow(ActionEvent actionEvent){
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.close();
+    }
+
+    static ResourceBundle getResourceBundle(){
+
+        // 1. Check for current locale
+        Locale loc = Locale.getDefault();
+
+        // 2. Get appropriate Resource Bundle
+        if(!loc.toLanguageTag().equals("en-US")){ // if locale is NOT english (default)
+
+            loc = new Locale("es", "ES"); // explicitly set locale to spanish
+        }
+
+        return ResourceBundle.getBundle("lang.login", loc);
+    }
+
+}// end App
--- src/controller/Login.java
+++ src/controller/Login.java
@@ -1,4 +1,61 @@
 package controller;
 
-public class Login {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+public class Login implements Initializable {
+
+    @FXML
+    private TextField userTextField;
+
+    @FXML
+    private TextField passTextField;
+
+    @FXML
+    private Button loginBtn;
+    @FXML
+    private Button exitBtn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        // 1. Define Locale
+        Locale loc = Locale.getDefault();
+
+        userTextField.setPromptText(resources.getString("username"));
+        passTextField.setPromptText(resources.getString("password"));
+        loginBtn.setText(resources.getString("login"));
+        exitBtn.setText(resources.getString("exit"));
+
+
+        // keep prompt until typing
+        userTextField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+        passTextField.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+
+        exitBtn.setCancelButton(true);// default button for esc keypress
+    }
+
+    public void clickLogin(ActionEvent actionEvent) {
+    }
+
+    public void clickExit(ActionEvent actionEvent) {
+        Optional<ButtonType> result = App.dialog(Alert.AlertType.CONFIRMATION,
+                "Exit Customer Scheduling" , "Confirm Exit Application",
+                "Are you sure you want to exit the application?\n\n");
+
+        if (result.isPresent() && result.get() == ButtonType.OK)
+            App.closeThisWindow(actionEvent);
+    }
+
+
 }
