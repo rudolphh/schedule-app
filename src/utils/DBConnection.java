414be9c3e15ec7adfb31c813e3149b93a6a52413
rudolphh <rudolpharthur@gmail.com>
2020-10-27 10:58:25 -0700

    add db connection options autoReconnect, useSSL false

--- src/utils/DBConnection.java
+++ src/utils/DBConnection.java
@@ -12,7 +12,8 @@ public class DBConnection {
     // JDBC URL parts
     private static final String protocol = rb.getString("protocol");
     private static final String vendorName = ":" + rb.getString("vendorName") + ":";
-    private static final String ipAddress = "//" + rb.getString("ipAddress") + "/" + rb.getString("dbName");
+    private static final String options = "?autoReconnect=true&useSSL=false";
+    private static final String ipAddress = "//" + rb.getString("ipAddress") + "/" + rb.getString("dbName") + options;
 
     // JDBC URL
     private static final String jdbcURL = protocol + vendorName + ipAddress;
