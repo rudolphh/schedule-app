df114f62e8b8d006874dbbe46c03cb7725e87ee2
Rudy A. Hernandez <rudolpharthur@gmail.com>
2021-01-29 22:11:31 -0800

    Start separation, entity and dto

---
+++ src/main/java/dto/AppointmentDto.java
@@ -0,0 +1,92 @@
+package dto;
+
+import java.time.LocalDateTime;
+
+public class AppointmentDto {
+
+    private int appointmentId;
+    private int customerId;
+    private int userId;
+    private String type;
+    private String userName;
+    private String customerName;
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    public AppointmentDto(int appointmentId, int customerId, int userId, String type, String userName, String customerName,
+                          LocalDateTime start, LocalDateTime end) {
+        this.appointmentId = appointmentId;
+        this.customerId = customerId;
+        this.userId = userId;
+        this.type = type;
+        this.userName = userName;
+        this.customerName = customerName;
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+}
---
+++ src/main/java/dto/UserDto.java
@@ -0,0 +1,52 @@
+package dto;
+
+public class UserDto {
+    private int id;
+    private String userName;
+    private String password;
+    private int active;
+
+    public UserDto(int id, String userName, String password, int active) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return getUserName();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+}
